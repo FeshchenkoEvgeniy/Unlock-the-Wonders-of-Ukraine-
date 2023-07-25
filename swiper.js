fc6343b3aeea5a147e054f5733118ad4e1f93f54
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-07-25 12:10:01 +0300

    added swiper for gallery

--- swiper.js
+++ swiper.js
@@ -11,3 +11,21 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".s-button-prev",
   },
 });
+
+var swiperGallery = new Swiper(".gallerySwiper", {
+  slidesPerView: 1,
+  breakpoints: {
+    835: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1440: {
+      slidesPerView: 3.5,
+      spaceBetween: 16,
+    }
+  },
+  navigation: {
+    nextEl: ".s-button-next",
+    prevEl: ".s-button-prev",
+  },
+});
