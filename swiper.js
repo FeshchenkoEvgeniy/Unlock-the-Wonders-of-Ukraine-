9a8dae6b9cb38f3332f12fcefc168508eb4d9491
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-07-31 22:39:34 +0300

    finish

--- swiper.js
+++ swiper.js
@@ -22,7 +22,25 @@ var swiperGallery = new Swiper(".gallerySwiper", {
     1440: {
       slidesPerView: 3.5,
       spaceBetween: 16,
-    }
+    },
+  },
+  navigation: {
+    nextEl: ".s-button-next",
+    prevEl: ".s-button-prev",
+  },
+});
+
+var swiperReviews = new Swiper(".reviewsSwiper", {
+  slidesPerView: 1,
+  breakpoints: {
+    835: {
+      slidesPerView: 2.37,
+      spaceBetween: 32,
+    },
+    1440: {
+      spaceBetween: 32,
+      slidesPerView: 4,
+    },
   },
   navigation: {
     nextEl: ".s-button-next",
